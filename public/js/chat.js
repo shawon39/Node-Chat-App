d340424ce94b43ceafdd655b24945ed49f41806d
shawon <shshawon39@gmail.com>
2018-06-22 15:09:34 +0600

    Message send specific room and project finished!

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@ jQuery('#message-form').on('submit', (e) => {
     var messageTextBox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, () => {
         messageTextBox.val('');
